791b8b2bc5107cedb0c4d3ddae9bfab04b2cffe5
W_CHOI <90023836+Choi-Master@users.noreply.github.com>
2024-11-19 13:08:21 -0800

    Update a5.py

--- assignments/a5/a5.py
+++ assignments/a5/a5.py
@@ -1,13 +1,13 @@
 # a5.py
 
 #
-# Write the full name, SFU ID number, and SFU email of each member of your team:
+# write down your full name and your partner's full name.
 #
 #
 
 
 #
-# The TA will run your program by calling the main() function. Inside main() put
+# The teacher will run your program by calling the main() function. Inside main() put
 # whatever code you need for your program to run. It should only be one or two
 # lines of code.
 #
